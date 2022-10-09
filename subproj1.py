16391b1d4d7c88272e0ec9fd2300291d2d6f2ac6
mike <mjcrow@gmail.com>
2022-10-09 15:13:26 -0500

    First assignment

---
+++ subproj1.py
@@ -0,0 +1,4 @@
+# This is the first assignment:
+# First, use range() and a while loop to sum the numbers from 1 to n (n=100 is a good default)
+# Secondly, derive and use a formula to calculate the sum of the numbers from 1 to n
+# Finally, use time functions to determine the difference in speed between the two methodologies
